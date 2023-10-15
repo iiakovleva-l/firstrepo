30fb503cdb2912242de2db9f7cee03ddc977735a
iiakovleva <ieliakovleva@gmail.com>
2023-10-15 18:56:16 +0200

    edit py file

--- pythonfiles/test_math.py
+++ pythonfiles/test_math.py
@@ -1,1 +1,15 @@
-print("Hello world")
+# Файл: test_math.py
+
+def add(x, y):
+    """Функция сложения двух чисел."""
+    return x + y
+
+def test_add_positive_numbers():
+    assert add(1, 2) == 3
+
+def test_add_negative_numbers():
+    assert add(-1, -1) == -2
+
+def test_add_mixed_numbers():
+    assert add(1, -1) == 0
+    assert add(0, 0) == 0
